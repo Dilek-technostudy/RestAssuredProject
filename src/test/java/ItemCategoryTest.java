da4e07e2f751872a86f9770bf83ddffedb2451b9
dilek-technostudy <dileknuranyildirim@gmail.com>
2020-03-02 16:19:57 -0800

    connection check

---
+++ src/test/java/ItemCategoryTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ItemCategoryTest {
+
+    @Test
+    public void createItem{
+        
+    }
+}
